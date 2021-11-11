1ed497c096467b73f1be5f3b8e3eb2f8be30c60b
rospiel <rospielberg@gmail.com>
2021-11-11 18:24:43 -0300

    add loaders de css e files

--- curso_origamid/mini_dog/webpack.config.js
+++ curso_origamid/mini_dog/webpack.config.js
@@ -2,6 +2,7 @@ module.exports = {
   /* onde o server deve buscar o html raiz */
   devServer: {
     static: './',
+    historyApiFallback: true,
   },
 
   module: {
@@ -15,6 +16,18 @@ module.exports = {
             presets: ['@babel/preset-react'],
           },
         },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
       },
     ],
   },
