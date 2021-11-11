50a361e4518eb0e78aacc0515aa7ca9585968bc8
rospiel <rospielberg@gmail.com>
2021-11-11 19:41:02 -0300

    add a custom hook for fetch data from server

---
+++ curso_origamid/mini_dog/src/Components/useFetch.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const useFetch = () => {
+  const [data, setData] = React.useState(null);
+  const [error, setError] = React.useState(null);
+  const [loading, setLoading] = React.useState(null);
+
+  const request = React.useCallback(async (url, options) => {
+    let response;
+    let json;
+    try {
+      setError(null);
+      setLoading(true);
+      response = await fetch(url, options);
+      json = await response.json();
+
+      if (response.ok === false) {
+        throw new Error(json.message);
+      }
+    } catch (err) {
+      json = null;
+      setError(err.message);
+    } finally {
+      setData(json);
+      setLoading(false);
+      return { response, json };
+    }
+  }, []);
+
+  /* disponibilizamos além dos dados o método request(url, options) */
+  return { data, loading, error, request };
+};
+
+export default useFetch;
