4617620dbfd6d17cec0288150a564c09a98f344c
rospiel <rospielberg@gmail.com>
2021-11-11 18:57:40 -0300

    Routes App and initial css App

--- curso_origamid/mini_dog/src/App.js
+++ curso_origamid/mini_dog/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-    <>
-      <p>Eu sou o App</p>
-    </>
+    <div className="App">
+      <BrowserRouter>
+        <div className="content">
+          <Routes></Routes>
+        </div>
+      </BrowserRouter>
+    </div>
   );
 };
 
