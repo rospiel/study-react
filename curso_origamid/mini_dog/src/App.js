fa7cd8c145231e68089cbee5d7062255a406b8c8
rospiel <rospielberg@gmail.com>
2021-11-11 19:07:00 -0300

    Using header component in the app

--- curso_origamid/mini_dog/src/App.js
+++ curso_origamid/mini_dog/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './Components/Header';
 
 const App = () => {
   return (
     <div className="App">
       <BrowserRouter>
+        <Header />
         <div className="content">
           <Routes></Routes>
         </div>
