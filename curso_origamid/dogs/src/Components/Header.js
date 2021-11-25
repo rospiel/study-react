6ba93004e8215361fd861b3bc8c1ca22d6672ff9
rospiel <rospielberg@gmail.com>
2021-11-25 19:10:59 -0300

    Using useContext on header

--- curso_origamid/dogs/src/Components/Header.js
+++ curso_origamid/dogs/src/Components/Header.js
@@ -2,17 +2,35 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Header.module.css';
 import { ReactComponent as Dogs } from '../Assets/dogs.svg';
+import { UserContext } from '../UserContext';
 
 const Header = () => {
+  const { data, userLogout } = React.useContext(UserContext);
+
+  function getLinkMyAccount() {
+    return (
+      <Link className={styles.login} to="/conta">
+        {data.nome}
+        <button onClick={userLogout}>Sair</button>
+      </Link>
+    );
+  }
+
+  function getLinkLogin() {
+    return (
+      <Link className={styles.login} to="/login">
+        Login / Criar
+      </Link>
+    );
+  }
+
   return (
     <header className={styles.header}>
-      <nav className={`${styles.nav} container`}>
+      <nav className={styles.nav}>
         <Link className={styles.logo} to="/" aria-label="Dogs - Home">
           <Dogs />
         </Link>
-        <Link className={styles.login} to="/login">
-          Login / Criar
-        </Link>
+        {data ? getLinkMyAccount() : getLinkLogin()}
       </nav>
     </header>
   );
