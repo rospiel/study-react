694098ebdc48985ac7738cac1c5ab1d7da7b20f9
rospiel <rospielberg@gmail.com>
2021-12-29 18:45:07 -0300

    implementation of components password lost and reset

--- curso_origamid/dogs/src/Components/Login/LoginPasswordLost.js
+++ curso_origamid/dogs/src/Components/Login/LoginPasswordLost.js
@@ -1,7 +1,60 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import Input from '../Forms/Input';
+import Button from '../Forms/Button';
+import useForm from '../../Hooks/useInputValidator';
+import useFetch from '../../Hooks/useFetch';
+import { PASSWORD_LOST } from '../../apiUrl';
+import Error from '../Helper/Error';
 
 const LoginPasswordLost = () => {
-  return <div>LoginPasswordLost</div>;
+  const email = useForm();
+  const { data, loading, error, request } = useFetch();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    if (!email.validate) {
+      return;
+    }
+
+    const { url, options } = PASSWORD_LOST({
+      login: email.value,
+      url: window.location.href.replace('perdeu', 'resetar'),
+    });
+    request(url, options);
+    disabledButton();
+  }
+
+  function disabledButton() {
+    return <Button disabled>Enviando</Button>;
+  }
+
+  function sendButton() {
+    return <Button>Enviar Email</Button>;
+  }
+
+  function informIfAlreadySend() {
+    return <p style={{ color: '#4c1' }}>{data}</p>;
+  }
+
+  function configureButtonSend() {
+    return loading ? disabledButton() : sendButton();
+  }
+
+  return (
+    <section>
+      <h1 className="title">Perdeu a senha?</h1>
+      {data ? (
+        informIfAlreadySend()
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <Input label="Email / Usuário" type="text" name="email" {...email} />
+          {configureButtonSend()}
+        </form>
+      )}
+
+      <Error error={error} />
+    </section>
+  );
 };
 
 export default LoginPasswordLost;
--- curso_origamid/dogs/src/Components/Login/LoginPasswordReset.js
+++ curso_origamid/dogs/src/Components/Login/LoginPasswordReset.js
@@ -1,7 +1,80 @@
 import React from 'react';
+import Input from '../Forms/Input';
+import Button from '../Forms/Button';
+import useForm from '../../Hooks/useInputValidator';
+import useFetch from '../../Hooks/useFetch';
+import { PASSWORD_RESET } from '../../apiUrl';
+import Error from '../Helper/Error';
+import { useNavigate } from 'react-router-dom';
 
 const LoginPasswordReset = () => {
-  return <div>LoginPasswordReset</div>;
+  const [login, setLogin] = React.useState('');
+  const [key, setKey] = React.useState('');
+  const password = useForm('required');
+  const { error, loading, request } = useFetch();
+  const navigate = useNavigate();
+
+  function configureKeyAndLogin(key, login) {
+    if (key) {
+      setKey(key);
+    }
+
+    if (login) {
+      setLogin(login);
+    }
+  }
+
+  React.useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const key = params.get('key');
+    const login = params.get('login');
+    configureKeyAndLogin(key, login);
+  }, []);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    if (!password.validate()) {
+      return;
+    }
+
+    const { url, options } = PASSWORD_RESET({
+      login,
+      key,
+      password: password.value,
+    });
+
+    const response = await request(url, options);
+    if (response.ok) {
+      navigate('/login');
+    }
+  }
+
+  function disabledButton() {
+    return <Button disabled>Enviando</Button>;
+  }
+
+  function sendButton() {
+    return <Button>Resetar</Button>;
+  }
+
+  function configureButtonReset() {
+    return loading ? disabledButton() : sendButton();
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <Input
+          label="Nova Senha"
+          type="password"
+          name="password"
+          {...password}
+        />
+        {configureButtonReset()}
+      </form>
+      <Error error={error} />
+    </div>
+  );
 };
 
 export default LoginPasswordReset;
