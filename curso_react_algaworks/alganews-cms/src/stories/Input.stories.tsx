3660d16b42b2d161c4851cc34368bf38d7e6eed9
rospiel <rospielberg@gmail.com>
2022-01-31 19:21:34 -0300

    Add input to storybook

---
+++ curso_react_algaworks/alganews-cms/src/stories/Input.stories.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta, Story } from '@storybook/react';
+
+import Input, { InputProps }  from '../components/Input/Input';
+
+export default {
+  title: 'Example/Input',
+  component: Input,
+  //argTypes: {
+    //backgroundColor: { control: 'color' },
+  //},
+} as ComponentMeta<typeof Input>;
+
+const Template: Story<InputProps> = (args) => <Input {...args} />;
+
+export const WithPlaceholder = Template.bind({})
+
+WithPlaceholder.args = {
+  placeholder: 'Example placeholder'
+}
+
+export const WithLabelAndPlaceholder = Template.bind({})
+
+WithLabelAndPlaceholder.args = {
+  label: 'Name',
+  placeholder: 'Example placeholder'
+}
+
+export const WithLabelPlaceholderAndContent = Template.bind({})
+
+WithLabelPlaceholderAndContent.args = {
+  label: 'Name',
+  placeholder: 'Example placeholder',
+  value: 'My name'
+}
+
+export const WithContent = Template.bind({})
+
+WithContent.args = {
+  value: 'My name'
+}
