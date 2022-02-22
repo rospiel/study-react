61b10ff44e0eb18756e36e362e01760905c3d3e4
rospiel <rospielberg@gmail.com>
2022-02-22 18:48:41 -0300

    feat: add component info for warning or inform something

---
+++ curso_react_algaworks/alganews-cms/src/components/Info/Info.tsx
@@ -0,0 +1,52 @@
+import Icon from "@mdi/react";
+import { mdiInformation } from "@mdi/js"; 
+import styled from "styled-components";
+
+export interface InfoProps {
+  title: string
+  description: string
+}
+
+export default function Info (props: InfoProps) {
+  return( 
+    <InfoWrapper>
+      <InfoIcon>
+        <Icon 
+          color="#09f"
+          size="48px"
+          path={mdiInformation} />
+      </InfoIcon>
+      <InfoMessages>
+        <InfoTitle>{props.title}</InfoTitle>
+        <p>{props.description}</p>
+      </InfoMessages>
+    </InfoWrapper>
+  )
+}
+
+const InfoWrapper = styled.div`
+  padding: 24px;
+  display: flex;
+  background-color: #F3F8FA;
+  width: 373px;
+`
+
+const InfoIcon = styled.div`
+  margin-right: 16px;
+`
+
+const InfoMessages = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+`
+
+const InfoTitle = styled.h2`
+  font-size: 24px;
+  font-weight: 500;
+  display: -webkit-box;
+  -webkit-line-clamp: 1;
+  -webkit-box-orient: vertical;
+  line-clamp: 1;
+  overflow: hidden;
+`
