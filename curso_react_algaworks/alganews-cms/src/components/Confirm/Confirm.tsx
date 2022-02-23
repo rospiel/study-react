1daad65e884d37728aca0c8c042312f2525ff9e3
rospiel <rospielberg@gmail.com>
2022-02-23 18:34:29 -0300

    feat: add confirm component

---
+++ curso_react_algaworks/alganews-cms/src/components/Confirm/Confirm.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import styled from "styled-components";
+import Button from "../Button/Button";
+
+export interface ConfirmProps {
+  question: string
+}
+
+export default function Confirm (props: ConfirmProps) {
+  return (
+    <Container>
+      <span className="question">{props.question}</span>
+      <Actions>
+        <Button label="Não" variant="danger"/>
+        <Button label="Sim" variant="primary"/>
+      </Actions>
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  width: 229px;
+  height: 106px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #F3F8FA;
+  gap: 12px;
+
+  span.question {
+    font-weight: bold;
+    font-size: 18px;
+    font-weight: 21.6px;
+    color: #274060;
+    display: -webkit-box;
+    -webkit-line-clamp: 1;
+    -webkit-box-orient: vertical;
+    line-clamp: 1;
+    overflow: hidden;
+  }
+
+`
+
+const Actions = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 8px;
+`
+
---
+++ curso_react_algaworks/alganews-cms/src/stories/Confirm.stories.tsx
@@ -0,0 +1,15 @@
+import { ComponentMeta, Story } from '@storybook/react';
+
+import Confirm, { ConfirmProps }  from '../components/Confirm/Confirm';
+
+export default {
+  title: 'Example/Confirm',
+  component: Confirm,
+} as ComponentMeta<typeof Confirm>;
+
+const Template: Story<ConfirmProps> = (args) => <Confirm {...args} />;
+
+export const Default = Template.bind({})
+Default.args = {
+  question: 'Deseja realmente sair?'
+}
