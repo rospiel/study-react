594f2851e660b5776424fd743957ff3f63b2f185
rospiel <rospielberg@gmail.com>
2022-04-11 18:36:18 -0300

    feat: add component circle chart

---
+++ curso_react_algaworks/alganews-cms/src/stories/CircleChart.stories.tsx
@@ -0,0 +1,34 @@
+import { ComponentMeta, Story } from '@storybook/react';
+
+import CircleChart, { CircleChartProps }  from '../components/CircleChart';
+
+export default {
+  title: 'Example/CircleChart',
+  component: CircleChart,
+  argTypes: {
+    progress: {
+      control: {
+        type: 'range',
+        min: 0,
+        max: 100
+      }
+    }
+  }
+} as ComponentMeta<typeof CircleChart>;
+
+const Template: Story<CircleChartProps> = (args) => <CircleChart {...args} />;
+
+export const Default = Template.bind({})
+Default.args = { 
+  progress: 80,
+  size: 150,
+  caption: "web"
+}
+
+export const Primary = Template.bind({})
+Primary.args = { 
+  progress: 80,
+  size: 150,
+  caption: "android",
+  theme: "primary"
+}
