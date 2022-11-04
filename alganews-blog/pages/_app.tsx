8b127af5da56362599342218700b4580bc4b714b
rospiel <rospielberg@gmail.com>
2022-11-04 09:57:59 -0300

    feat.: add global styles and fonts

--- alganews-blog/pages/_app.tsx
+++ alganews-blog/pages/_app.tsx
@@ -2,11 +2,13 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from "styled-components";
 import { light } from '../styles/theme';
+import GlobalStyles from '../styles/globalStyles';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={light}>
       <Component {...pageProps} />
+      <GlobalStyles />
     </ThemeProvider> 
   );
 }
