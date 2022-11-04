4e9c3c579a873cd256afc44a61732a194aae7257
rospiel <rospielberg@gmail.com>
2022-11-04 09:36:49 -0300

    feat.: creating a theme

--- alganews-blog/components/Test.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export default function Test() {
-  return <Title>OLA</Title>
-}
-
-const Title = styled.h1`
-  color: red;
-`
--- alganews-blog/pages/_app.tsx
+++ alganews-blog/pages/_app.tsx
@@ -1,6 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from "styled-components";
+import { light } from '../styles/theme';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={light}>
+      <Component {...pageProps} />
+    </ThemeProvider> 
+  );
 }
